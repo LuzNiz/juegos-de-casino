f768a76b75a8b7d01f70e5e74477830334a55f30
LuzNiz <luzniz37@gmail.com>
2023-04-24 19:03:45 -0300

    Arreglando bug

--- src/tragamonedaEstandar.ts
+++ src/tragamonedaEstandar.ts
@@ -9,7 +9,19 @@ export class TragamonedaEstandar extends Tragamonedas {
     constructor(nombre: string, apuestaMinima: number) {
         super(nombre, apuestaMinima);
     }
-    
+    public girar(): void {
+        const resultado1 = Math.floor(Math.random() * 5);
+        const resultado2 = Math.floor(Math.random() * 5);
+        const resultado3 = Math.floor(Math.random() * 5);
+        this.numeros.push(resultado1, resultado2, resultado3);
+
+        console.log(`
+        ----------
+        ${color.red(resultado1)} | ${color.green(resultado2)} | ${color.blue(resultado3)} |
+        ----------
+        `);
+    }
+
     public esGanador(): number {
         let resultado = '';
         if (this.numeros[0] === this.numeros[1] && this.numeros[1] === this.numeros[2]){ 
--- src/tragamonedas.ts
+++ src/tragamonedas.ts
@@ -9,16 +9,5 @@ export abstract class Tragamonedas extends Juego{
       this.numeros = [];
     }
     
-    protected girar(): void {
-      const resultado1 = Math.floor(Math.random() * 5);
-      const resultado2 = Math.floor(Math.random() * 5);
-      const resultado3 = Math.floor(Math.random() * 5);
-      this.numeros.push(resultado1, resultado2, resultado3);
-
-      console.log(`
-      ----------
-      ${color.red(resultado1)} | ${color.green(resultado2)} | ${color.blue(resultado3)} |
-      ----------
-      `);
-    }
+    public abstract girar(): void;
 }
